858cfbb19680e9451b8cac6e31122b171ca42992
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-05-20 13:49:55 +0200

    set cranlog install to quiet

--- packages.R
+++ packages.R
@@ -24,4 +24,4 @@ utils::install.packages(
 # remotes::install_github(repo = "gabraham/flashpca/flashpcaR", quiet = TRUE)
 remotes::install_github(repo = "dreamRs/prefixer", quiet = TRUE)
 remotes::install_github(repo = "rstudio/gt", quiet = TRUE)
-remotes::install_github("metacran/cranlogs")
+remotes::install_github("metacran/cranlogs", quiet = TRUE)
